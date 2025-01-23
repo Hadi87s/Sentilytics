a4c32952f3db542d2d5f843feda31b0cd367805d
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2025-01-23 17:28:46 +0200

    changing the font to Poppins.

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import { fontFamily } from "tailwindcss/defaultTheme";
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       animation: {
-        'bounce-slow': 'bounce 3s infinite',
+        "bounce-slow": "bounce 3s infinite",
       },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        sans: ["Poppins", ...fontFamily.sans], // Add Poppins as the default sans font
       },
     },
   },
